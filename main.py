5aca38c03a8cb488db1c11f12ab764e620cba372
themercantile <mercantile.code@gmail.com>
2020-05-18 23:44:39 +0800

    Now added database functionality. Working locally.

--- main.py
+++ main.py
@@ -5,11 +5,21 @@
 
 from flask import Flask, render_template, request, session
 from coffeecalc import MyCoffee
+#from datetime import datetime
+import datetime
 import pymongo
 import os
 
-# basedir = os.path.abspath(os.path.dirname(__file__))
-# SECRET_KEY = os.environ.get("SECRET_KEY") or "I_<3_Rusty_26-04-2020"
+#connecting to my mongodb atlas db
+'''
+
+'''
+myClient = pymongo.MongoClient("mongodb://localhost:27017/")
+
+mydb = myClient["coffeedatabase"]
+# "coffee_orders" is what the database will be called on the server
+coffeeDocument = mydb["coffee_orders"]
+# "coffeeDocument" is a json document used in pymongo database
 
 app = Flask(__name__)
 
@@ -23,7 +33,8 @@ def index():
 @app.route("/finalise", methods=["GET", "POST"])
 def finalise():
   myTupleList=session['orderTuple']
-  print(myTupleList)
+  print(myTupleList) # this is for debugging
+  dbWrite(myTupleList)
   return render_template("finalise.html", coffeeTuple=myTupleList)
 
 @app.route("/order", methods=["GET", "POST"])
@@ -49,7 +60,24 @@ def order():
     return render_template("coffeelist.html", coffeeTuple = myTupleList)    
   else:
     myTupleList = []
-    return render_template("order.html")
+    # Lines with time functions are to determine if it's too late to make an order. Will allow midnight to 9.59AM
+    now = datetime.datetime.now()
+    if (int(now.strftime("%H")) > 23):
+      return "Orders end at 9.59AM - Sorry!"
+    else:
+      return render_template("order.html")
+
+# write to database
+def dbWrite(myTupleList):
+  coffeeDic = []
+  now = datetime.datetime.now()
+  nowstr = now.strftime("%Y%m%d")  # E.g. 20200518 <-- 2020 05 18
+  for myTuple in myTupleList:
+    #create list of dics to write to db
+    coffeeDic.append({"date": nowstr, "name": myTuple[0], "email": myTuple[1], "drinktype": myTuple[2], "milkType": myTuple[3], 
+                      "sugarAmt": myTuple[4], "extras": myTuple[5], "drinkSize": myTuple[6]})
+  # print(coffeeDic) for debugging purposes
+  x = coffeeDocument.insert_many(coffeeDic) #write to database
 
 if __name__ == "__main__":
     app.run(debug=True)
